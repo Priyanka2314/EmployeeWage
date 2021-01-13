4442c4031c794161b0370d39d395ecad1577708d
Priyanka <priyankacs102@gmail.com>
2021-01-14 01:11:50 +0530

    Added: Simple Java Statement

---
+++ src/employeewage/EmpWage.java
@@ -0,0 +1,11 @@
+package employeewage;
+
+public class EmpWage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Calcalute Emp Wage");
+
+	}
+
+}
