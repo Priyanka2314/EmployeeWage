a4003016408222a6f6611e3c2be0b338d09c3eeb
Priyanka <priyankacs102@gmail.com>
2021-01-14 01:23:31 +0530

    Added: Code to check employee attendence

--- src/employeewage/EmpWage.java
+++ src/employeewage/EmpWage.java
@@ -3,8 +3,13 @@ package employeewage;
 public class EmpWage {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Calcalute Emp Wage");
+		//Constants
+		int IS_FULL_TIME =1;
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if(empCheck == IS_FULL_TIME)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
 
 	}
 
