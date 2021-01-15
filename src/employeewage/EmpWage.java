ceeaa1a28ffb041acffd7e0e62f411561e8cc669
Priyanka <priyankacs102@gmail.com>
2021-01-15 23:36:08 +0530

    MODIFIED: code to calulate emp wage till 20 days or 100 hrs reached using while loop

--- src/employeewage/EmpWage.java
+++ src/employeewage/EmpWage.java
@@ -5,15 +5,18 @@ public class EmpWage {
 		public static final int IS_PART_TIME = 1;
 		public static final	int IS_FULL_TIME = 2;
 		public static final	int EMP_RATE_PER_HOUR = 20;
-		public static final	int NUM_OF_WORKING_DAYS = 10;
-				;
+		public static final	int NUM_OF_WORKING_DAYS = 20;
+		public static final int MAX_HRS_IN_MONTH = 100;
+				
 
 	public static void main(String[] args) {
 		//variables
-		int empHrs = 0;
 		int empWage = 0;
-		int totalWage = 0;
-		for(int day = 0;day < NUM_OF_WORKING_DAYS ; day++) {
+		int totalEmpWage = 0;
+		int totalEmpHrs = 0,totalWorkingDays = 0;
+		while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS ) {
+			int empHrs = 0;
+			totalWorkingDays++;
 		 int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 	     switch (empCheck) {
 		 case IS_PART_TIME :
@@ -25,14 +28,16 @@ public class EmpWage {
 		 default :
 			empHrs = 0;
 	   }
-	
+	    totalEmpHrs +=empHrs;
+	    System.out.println("Day#: "+totalWorkingDays+ " Emp Hr: "+empHrs);
 		empWage = empHrs * EMP_RATE_PER_HOUR ;
-		totalWage += empWage ;
+		totalEmpWage += empWage ;
 		System.out.println("Emp Wage : "+empWage);
 		
 		
 	}
-	   System.out.println("Total Emp Wage: "+totalWage);
+		
+	   System.out.println("Total Emp Wage: "+totalEmpWage);
 
 	}
 
